0629efdee87692fb4395260236adb98ba1ebb357
MS_CrySis21 <SHASHANKMS2003@GMAIL.COM>
2023-07-09 18:11:37 +0530

    Update chessPawns.java

--- chessPawns.java
+++ chessPawns.java
@@ -1,18 +1,19 @@
 import java.util.LinkedList;
 
 public class chessPawns {
-    int xp;
+    //Giving the pawns there axes to be placed
+    int xp;   
     int yp;
     int x;
     int y;
-    boolean isWhite;
+    boolean isWhite;    
     String name;
     LinkedList<chessPawns> ps;
     public chessPawns(int xp, int yp, boolean isWhite ,String n, LinkedList<chessPawns> ps){
         this.xp = xp ;
         this.yp = yp;
-        x = xp * 64;
-        y = yp * 64;
+        x = xp * 64;  //Reading every rows and columns x = row 
+        y = yp * 64;  // y = column
         this.isWhite = isWhite;
         this.ps = ps;
         name = n;
@@ -20,15 +21,17 @@ public class chessPawns {
     }
 
     public void move(int xp , int yp){
-        if(chessBoard.getchessPawns(xp * 64, yp * 64) != null) {
-            if (chessBoard.getchessPawns(xp * 64, yp * 64).isWhite != isWhite) {
-                chessBoard.getchessPawns(xp * 64, yp * 64).kill();
+        if(chessBoard.getchessPawns(xp * 64, yp * 64) != null) {   
+            if (chessBoard.getchessPawns(xp * 64, yp * 64).isWhite != isWhite) {  //if pawns are white then it can't be killed if not it can be killed
+                chessBoard.getchessPawns(xp * 64, yp * 64).kill();   //kill the pawns if they are placed on different colour
             }else{
-                x = this.xp * 64;
+                //if not they move withtout any killing
+                x = this.xp * 64;  
                 y = this.yp * 64;
                 return ;
             }
         }
+        //updating the positions after movement
         this.xp = xp;
         this.yp = yp;
         x = xp * 64;
